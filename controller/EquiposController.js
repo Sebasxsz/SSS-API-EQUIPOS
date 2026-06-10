adacaf1c82b33a0fcb1ad768cf1333574493c5c7
Steven Sabando <sebastiansabando2005@gmail.com>
2026-06-09 19:30:23 -0500

    Cree el método de obtener todos los equipos

--- controller/EquiposController.js
+++ controller/EquiposController.js
@@ -1,4 +1,12 @@
 import {Equipo} from '../model/EquiposModel.js'
 
+export const getEquipos = async (req, res) => {
+    try {
+        const equipos = await Equipo.findAll()  
+        return res.json(equipos)
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al obtener los equipos' })
+    }
+}
 
 
